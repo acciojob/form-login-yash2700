e45d5f7a220fe69682b5eb8e331e6ba78a29ae7c
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-05-29 13:32:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+	var fname=document.getElementById("fname");
+	var lname=document.getElementById("lname");
+	alert(fname.value+" "+lname.value)
 }
+
+
